e8f2ab5ad9a36499fd02c54f05af469fb38bfc8c
Ibrahim <ibra.rodriguez.olaya@gmail.com>
2025-01-12 21:15:15 -0600

    secret.json

--- inventario/applications/dispositivo/apps.py
+++ inventario/applications/dispositivo/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DispositivoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dispositivo'
+    name = 'applications.dispositivo'
--- inventario/inventario/settings/local.py
+++ inventario/inventario/settings/local.py
@@ -10,7 +10,18 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_secret('DB_NAME'),
+        'USER': get_secret('USER'),
+        'PASSWORD': get_secret('PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
